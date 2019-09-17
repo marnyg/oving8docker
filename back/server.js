7382bfd9718b46afa89f0c4b304a36ef3fd05a1c
marius <marnyg31.10@gmail.com>
2019-09-17 17:19:54 +0200

    finished example password encryptin

--- back/server.js
+++ back/server.js
@@ -2,19 +2,17 @@ import express from 'express';
 import reload from 'reload';
 import fs from 'fs';
 import path from 'path';
+import * as CryptoJS from 'crypto-js';
 
-//type Request = express$Request;
-//type Response = express$Response;
+type Request = express$Request;
+type Response = express$Response;
 
 const public_path = path.join(__dirname, '/share');
 
 let app = express();
-
 app.use(express.static(public_path));
+app.use(express.json()); // For parsing application/json
 
-app.get('/', (req, res) => {
-    return res.send('Hello worlaaaad');
-});
 
 if (process.env.NODE_ENV !== 'production') {
   let reloadServer = reload(app);
@@ -24,4 +22,26 @@ if (process.env.NODE_ENV !== 'production') {
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
+});
+
+var users={
+	"ola" : { salt:"123123123123123123123123123", hash: '2b48d9685ee7e1486792ac603b70f8b91da12fb8b68cad61252c336c4888e792489322721104ca264b96c4c0c430d51253da5427675a07f97e56e94c71986821'}//password: aaa	
+}
+
+function myEncrypt(text){
+	var salt = CryptoJS.lib.WordArray.random(128/8);
+	var key512Bits1000Iterations = CryptoJS.PBKDF2(text, users["ola"].salt, { keySize: 512/32, iterations: 1000 });	
+	console.log("encrypted to ",String(key512Bits1000Iterations ));
+	console.log(users);
+	return users["ola"].hash===String(key512Bits1000Iterations)
+}
+
+app.put('/pass', (req: Request, res: Response) => {
+	let message=req.body.enc
+	console.log("got message ",message);
+	if(myEncrypt(message)){
+		return res.send({token:12312312313});
+	}else{
+		return res.send({status:"no, u suc"});
+	}
 });
--- front/index.js
+++ front/index.js
@@ -1,10 +1,44 @@
 import ReactDOM from 'react-dom';
+import * as CryptoJS from 'crypto-js';
 import * as React from 'react';
 import { Component } from 'react-simplified';
 import { HashRouter, Route } from 'react-router-dom';
+import axios from 'axios';
+axios.interceptors.response.use(response => response.data);
 
 
-ReactDOM.render(
-  <h1>Hasdelliiiio, world!</h1>,
-  document.getElementById('root')
-);
+class Password extends Component {
+
+    pass=''
+
+    render(){
+        return (
+            <div>
+                <h1>Hasdelliiiio, world!</h1>
+                <input ref="pass"></input>
+                <button onClick={this.handleStuff}>yo</button>
+            </div>
+        )
+    }
+    
+    handleStuff(event){
+        let inp=this.refs.pass
+        this.pass=inp.value
+        console.log(this.pass)
+        let encrypt=this.encrypt(this.pass)
+        this.sendToBackend(encrypt)
+    }
+
+    encrypt(purePass){
+        console.log("encrypting ", purePass)
+ 		let salt = "ola sitt salt";
+		let key128Bits = CryptoJS.PBKDF2(purePass, salt, { keySize: 128/32 });
+        return key128Bits 
+    }
+    sendToBackend(encryptedPass){
+        console.log("sending ", String(encryptedPass))
+		let enc=String(encryptedPass)
+		axios.put('/pass', {enc}).then(e=>console.log(e)).catch(e=>console.log(e))
+    }
+}
+ReactDOM.render( <Password/>, document.getElementById('root'));
